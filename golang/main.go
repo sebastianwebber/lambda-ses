b3e7db0527ec87e13273d7efe63c64f14af40132
Sebastian Webber <sebastian@swebber.me>
2019-03-26 11:56:37 -0300

    first commit

---
+++ golang/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, sesEvent events.SimpleEmailEvent) error {
+	for _, record := range sesEvent.Records {
+		ses := record.SES
+		fmt.Printf("[%s - %s] Mail = %+v, Receipt = %+v \n", record.EventVersion, record.EventSource, ses.Mail, ses.Receipt)
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
